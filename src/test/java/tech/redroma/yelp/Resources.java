6f3b5446c0d47893b1fe4ff87212029c2ac9030e
Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-30 14:01:11 -0800

    Adds Resources class to load files for testing purposes

---
+++ src/test/java/tech/redroma/yelp/Resources.java
@@ -0,0 +1,54 @@
+
+/*
+ * Copyright 2016 RedRoma, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.redroma.yelp;
+
+import java.io.IOException;
+import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+
+import static com.google.common.base.Charsets.UTF_8;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Internal
+public class Resources
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(Resources.class);
+
+    public static String loadResource(String name) throws IOException
+    {
+        URL url;
+        try
+        {
+            url = com.google.common.io.Resources.getResource(Resources.class, name);
+        }
+        catch (IllegalArgumentException ex)
+        {
+            LOG.error("Failed to {} load resource called [{]]", name, ex);
+            throw ex;
+        }
+
+        return com.google.common.io.Resources.toString(url, UTF_8);
+
+    }
+}

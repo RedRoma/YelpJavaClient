0c89a3bf35295310f52ccb7f97b718d6cb743230
Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-30 14:50:13 -0800

    Adds Yelp Business Details

---
+++ src/main/java/tech/redroma/yelp/YelpBusinessDetails.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2016 RedRoma, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.redroma.yelp;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+import java.util.Objects;
+import tech.sirwellington.alchemy.annotations.concurrency.Mutable;
+import tech.sirwellington.alchemy.annotations.concurrency.ThreadUnsafe;
+import tech.sirwellington.alchemy.annotations.objects.Pojo;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Pojo
+@Mutable
+@ThreadUnsafe
+public class YelpBusinessDetails
+{
+
+    public String id;
+
+    public String name;
+
+    @SerializedName("image_url")
+    public String imageURL;
+
+    @SerializedName("is_claimed")
+    public Boolean isClaimed;
+
+    @SerializedName("is_closed")
+    public Boolean isClosed;
+
+    public String url;
+
+    public String price;
+
+    public int rating;
+
+    public int reviewCount;
+
+    public String phone;
+
+    @SerializedName("photos")
+    public List<String> photosURLS;
+
+    public Hours hours;
+
+    public List<Category> categories;
+
+    public Coordinate coordinates;
+
+    public Address location;
+
+    
+    
+    @Pojo
+    @ThreadUnsafe
+    @Mutable
+    public static class Hours
+    {
+
+        public enum Type
+        {
+            REGULAR
+        }
+
+        public static class OpenTimes
+        {
+
+            public int day;
+            public String start;
+            public String end;
+            @SerializedName("is_overnight")
+            public Boolean isOvernight;
+
+            @Override
+            public int hashCode()
+            {
+                int hash = 7;
+                hash = 71 * hash + this.day;
+                hash = 71 * hash + Objects.hashCode(this.start);
+                hash = 71 * hash + Objects.hashCode(this.end);
+                hash = 71 * hash + Objects.hashCode(this.isOvernight);
+                return hash;
+            }
+
+            @Override
+            public boolean equals(Object obj)
+            {
+                if (this == obj)
+                {
+                    return true;
+                }
+                if (obj == null)
+                {
+                    return false;
+                }
+                if (getClass() != obj.getClass())
+                {
+                    return false;
+                }
+                final OpenTimes other = (OpenTimes) obj;
+                if (this.day != other.day)
+                {
+                    return false;
+                }
+                if (!Objects.equals(this.start, other.start))
+                {
+                    return false;
+                }
+                if (!Objects.equals(this.end, other.end))
+                {
+                    return false;
+                }
+                if (!Objects.equals(this.isOvernight, other.isOvernight))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public String toString()
+            {
+                return "OpenTimes{" + "day=" + day + ", start=" + start + ", end=" + end + ", isOvernight=" + isOvernight + '}';
+            }
+
+        }
+
+        public Type hoursType;
+
+        @SerializedName("is_open_now")
+        public Boolean isOpenNow;
+
+    }
+
+}
